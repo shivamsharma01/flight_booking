c4a8caa475425f765bb3e1f9ff8606b6873959ba
Shivam Sharma <shivam20121@iiitd.ac.com>
2020-09-16 16:50:18 +0530

    added fields payment_method and booking_status

--- python/root.py
+++ python/root.py
@@ -24,7 +24,7 @@ def connectdb():
     if x[0] == 'booking_table':
       found = True
   if found == False:
-    mycursor.execute("CREATE TABLE BOOKING_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), src_location VARCHAR(100), dest_location VARCHAR(100), class CHAR(1), travel_date DATETIME)")
+    mycursor.execute("CREATE TABLE BOOKING_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), src_location VARCHAR(100), dest_location VARCHAR(100), class CHAR(1), booking_status VARCHAR(100), payment_method VARCHAR(100), travel_date DATETIME)")
   
 
 connectdb()
@@ -35,7 +35,7 @@ def root():
   def booking(data):
     now = datetime.now()
     formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
-    sql = 'insert into BOOKING_TABLE(name, src_location, dest_location, class, travel_date) VALUES (%s, %s, %s, %s, %s)'
+    sql = 'insert into BOOKING_TABLE(name, src_location, dest_location, class, booking_status, payment_method, travel_date) VALUES (%s, %s, %s, %s, "PENDING", "PENDING", %s)'
     val = (data['name']+formatted_date, data['src_location'], data['dest_location'], data['class'], data['travel_date'])
     mycursor.execute(sql, val)
     mydb.commit()
