b86ad18dbc50e7fe243993a6cca20d729e6861ae
Shivam Sharma <shivam20121@iiitd.ac.com>
2020-10-02 23:52:25 +0530

    confirmed booking

--- python/root.py
+++ python/root.py
@@ -10,6 +10,7 @@ mycursor = None
 
 
 def connectdb():
+
     print("starting db")
     global mydb
     global mycursor
@@ -23,8 +24,9 @@ def connectdb():
     mycursor.execute("SHOW TABLES")
 
     dictionary = {
-        'booking_table': "CREATE TABLE BOOKING_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), src_location VARCHAR(100), dest_location VARCHAR(100), class CHAR(1), booking_status VARCHAR(100), payment_method VARCHAR(100), travel_date DATE)",
-        'schedule_table': "CREATE TABLE SCHEDULE_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, capacity INT UNSIGNED, src_location VARCHAR(100), dest_location VARCHAR(100), travel_date DATE)",
+        'booking_table': "CREATE TABLE BOOKING_TABLE (booking_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), src_location VARCHAR(100), dest_location VARCHAR(100), class CHAR(1), booking_status VARCHAR(100), payment_method VARCHAR(100), card_number INT UNSIGNED, travel_date DATE, flight_id INT UNSIGNED)",
+        'schedule_table': "CREATE TABLE SCHEDULE_TABLE (flight_id INT AUTO_INCREMENT PRIMARY KEY, src_location VARCHAR(100), dest_location VARCHAR(100), travel_date DATE)",
+        'credit_card_table': "CREATE TABLE CREDIT_CARD_TABLE (card_number INT AUTO_INCREMENT PRIMARY KEY, balance INT)",
     }
 
     tables = []
@@ -37,15 +39,40 @@ def connectdb():
 
 @api.route('/flightbooking/', methods=["POST"])
 def root():
+    price = {'B': 10000, 'E': 5000, 'F': 20000}
+
+    def date_converter(date):
+        return dateutil.parser.parse(date).date()
+
+    def is_past_date(date):
+        return date < datetime.now().date()
+
+    def get_no_of_passengers(flight_id):
+        sql = "SELECT count(*) FROM BOOKING_TABLE WHERE flight_id=" + \
+            str(flight_id)
+        mycursor.execute(sql)
+        record = mycursor.fetchone()
+        return record[0]
+
+    def delete_flight_schedule(flight_id):
+        sql = "DELETE FROM SCHEDULE_TABLE WHERE flight_id="+str(flight_id)
+        mycursor.execute(sql)
+        mydb.commit()
+
     def is_booked(id):
-      print(id)
-      sql = "SELECT * FROM BOOKING_TABLE WHERE id="+str(id)
-      mycursor.execute(sql)
-      record = mycursor.fetchone()
-      if mycursor.rowcount == 0:
-        return False, record
-      else:
-        return True, record
+        sql = "SELECT * FROM BOOKING_TABLE WHERE booking_id="+str(id)
+        mycursor.execute(sql)
+        record = mycursor.fetchone()
+        if mycursor.rowcount == 0:
+            return False, 0
+        else:
+            return True, record
+
+    def confirm_booking(id, card_number):
+        sql = "UPDATE BOOKING_TABLE SET booking_status=%s, payment_method=%s, card_number=%s WHERE booking_id=%s" 
+        mycursor.execute(sql, ("CONFIRMED", "CREDIT_CARD", card_number, id))
+        mydb.commit()
+        return mycursor.rowcount
 
     def is_Scheduled(src, dest, date):
         sql = "SELECT * FROM SCHEDULE_TABLE WHERE src_location=%s AND dest_location=%s AND travel_date=%s"
@@ -54,58 +81,143 @@ def root():
         if mycursor.rowcount == 0:
             return False, 0
         else:
-            return True, record[1]
+            return True, record
 
     def Schedule_flight(src, dest, date, request_type):
-        status, capacity = is_Scheduled(src, dest, date)
-        if status == False:
-            sql = 'insert into SCHEDULE_TABLE(src_location, dest_location, capacity, travel_date) VALUES (%s, %s, %s, %s)'
-            mycursor.execute(sql, (src, dest, 49, date))
-        else:
-            sql = "UPDATE SCHEDULE_TABLE SET capacity =%s WHERE src_location=%s AND dest_location=%s AND travel_date=%s"
-            if request_type == 'booking':
-              capacity = capacity - 1
-            else:
-                capacity = capacity + 1
-            mycursor.execute(sql, (capacity, src, dest, date))
-        mydb.commit()
-        if status == False:
-            print("{}{}".format("Scheduled-", mycursor.lastrowid))
-        else:
-            print("Updated Capacity to {}".format(capacity))
+        status, record = is_Scheduled(src, dest, date)
+        if (request_type == 'booking'):
+            if status == False:
+                sql = 'insert into SCHEDULE_TABLE(src_location, dest_location, travel_date) VALUES (%s, %s, %s)'
+                mycursor.execute(sql, (src, dest, date))
+                mydb.commit()
+                status, record = is_Scheduled(src, dest, date)
+            return record[0]
+        return status
 
+    def make_transaction(booking_id, card_number, amount, t_type):
+        print("{} {} {} {}".format(booking_id, card_number, amount, t_type))
+        sql = "SELECT * FROM CREDIT_CARD_TABLE WHERE card_number="+str(card_number)
+        mycursor.execute(sql)
+        record = mycursor.fetchone()
+        if mycursor.rowcount == 0:
+            return False, "This credit card is invalid!!!"
+        else:
+            sql = "UPDATE CREDIT_CARD_TABLE SET balance = %s WHERE card_number=%s"
+            if t_type == 'credit':
+                amount = int(record[1]) + int(amount)
+                mycursor.execute(sql, (amount, card_number))
+                mydb.commit()
+                return True, 'Rs.{} refunded.'.format(amount)
+            else:
+                amount = int(record[1]) - int(amount)
+                mycursor.execute(sql, (amount, card_number))
+                mydb.commit()
+                confirm_booking(booking_id, card_number)
+                return True, "Booking Confirmed"
+    '''
+
+    request payload 
+    {
+        "type": "booking",
+        "data": {
+            "name": "Fourth",
+            "src_location": "Delhi",
+            "dest_location": "Mumbai",
+            "class": "B",
+            "travel_date": "2020-09-27 15:37:53.706358"
+        }
+    }
+
+    response error
+        travel date Cannot be older than current date
+        
+    response success
+    {
+        "FLY-XXXX" //XXXX is the booking id
+    }
+    '''
     def booking(data):
-        date = dateutil.parser.parse(data['travel_date']).date()
-        if date < datetime.now().date():
+        date = date_converter(data['travel_date'])
+        if is_past_date(date) == True:
             return "travel date Cannot be older than current date"
-        Schedule_flight(data['src_location'], data['dest_location'], date, 'booking')
-        # print(status)
-        sql = 'insert into BOOKING_TABLE(name, src_location, dest_location, class, booking_status, payment_method, travel_date) VALUES (%s, %s, %s, %s, "PENDING", "PENDING", %s)'
-        val = (data['name'], data['src_location'], data['dest_location'],
-               data['class'], dateutil.parser.parse(data['travel_date']).date())
-        mycursor.execute(sql, val)
+        flight_id = Schedule_flight(
+            data['src_location'], data['dest_location'], date, 'booking')
+        sql = 'insert into BOOKING_TABLE(name, src_location, dest_location, class, booking_status, payment_method, travel_date, flight_id) VALUES (%s, %s, %s, %s, "PENDING", "PENDING", %s, %s)'
+        mycursor.execute(sql, (data['name'], data['src_location'], data['dest_location'],
+                               data['class'], date, flight_id))
         mydb.commit()
         return "{}{}".format("FLY-", mycursor.lastrowid)
 
+    '''
+
+    request payload 
+    {
+        "type": "cancel",
+        "data": {
+            "id": "3"
+        }
+    }
+
+    response error
+        "No reservation Available. Please check the booking id"
+        "Flight could not be canceled"
+        
+    response success
+    {
+        "Flight with bookingid X canceled"
+    }
+    '''
+
     def cancel(data):
-      id = data['id']
-      status, record = is_booked(id)
-      if status == False:
-        return "No reservation Available. Please check the booking id"
-      else:
-        sql = "DELETE FROM BOOKING_TABLE WHERE id="+str(id)
-        Schedule_flight(record[2], record[3], record[7], 'cancel')
+        id = data['booking_id']
+        status, record = is_booked(id)
+        if status == False:
+            return "No reservation Available. Please check the booking id"
+        
+        msg = ''
+        if (record[5] == 'CONFIRMED'):
+            card_number = int(record[7])
+            print(card_number)
+            amount = price[record[4]]*.5
+            status, msg = make_transaction(id, card_number, amount, 'credit')
+        flight_id = record[9]
+        passengers = get_no_of_passengers(flight_id)
+        if passengers == 1:
+            delete_flight_schedule(flight_id)
+        sql = "DELETE FROM BOOKING_TABLE WHERE booking_id="+str(id)
         mycursor.execute(sql)
         mydb.commit()
         if mycursor.rowcount == 0:
-          return "Flight could not be canceled"
+            return "Flight could not be canceled"
         else:
-          return "Flight with bookingid {} canceled".format(id)
+            return "Flight with bookingid {} canceled. {}".format(id, msg)
+
+    def confirm_payment(data):
+        id = data['booking_id']
+        card_number = data['card_number']
+        status, record = is_booked(id)
+        if status == False:
+            return "No reservation Available. Please check the booking id"
+        elif (record[5] == 'CONFIRMED'):
+            return "This Flight is already Confirmed!!!"
+        else:
+            flight_class = record[4]
+            amount = price[flight_class]
+            status, message = make_transaction(id, card_number, amount, 'debit')
+            return message
 
     if request.json['type'] == "booking":
         return booking(request.json['data'])
     elif request.json['type'] == "cancel":
         return cancel(request.json['data'])
+    elif request.json['type'] == "payment":
+        return confirm_payment(request.json['data'])
+    # elif request.json['type'] == "update-date":
+    #     return change_date(request.json['data'])
+    # elif request.json['type'] == "add-on":
+    #     return cancel(request.json['data'])
+    # elif request.json['type'] == "add-on":
+    #     return cancel(request.json['data'])
     else:
         return "Failed"
 
